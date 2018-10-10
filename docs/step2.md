21008e0ed72f2ad90ed7c944d345c61150db1d97
Rohit Mohan <rohit.mohan@gmail.com>
2018-10-10 09:30:15 -0400

    basic API server; beginnings of documentation

---
+++ docs/step2.md
@@ -0,0 +1,32 @@
+# Step 2 &mdash; The API Layer
+
+There is now a functional database with a live model. The next thing to do it to write an API to access that model.
+
+## What's an API
+
+## The Books API
+
+install express
+installing nodemon
+
+get /books
+get /books/:id
+post /books (json payload)
+  body parser
+  validations:
+    show validations in request
+    show validations in middleware
+
+put /books/:id (json)
+delete /books/:id
+
+- ensure json payload (middleware)
+- validate json payload for post
+### write the api
+
+
+### test the api with curl
+
+### test the api with postman
+
+### switch to web application
--- main.js
+++ main.js
@@ -1,8 +1,60 @@
 require("dotenv").config();
 
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const models = require("./models");
 
-const Book = models.Book;
+const app = express();
 
-var newBook = new Book({title: "Harry Potter and the Goblet of Fire"});
-newBook.save();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/books", async (request, response) => {
+    var books = await models.Book.findAll();
+
+    response.send({ books: books });
+});
+
+app.get("/books/:id", async (request, response) => {
+    var book = await models.Book.findById(parseInt(request.params.id));
+
+    book ? response.send(book) : response.status(404).send("Not found");
+});
+
+app.post("/books", async (request, response) => {
+    if (!request.body.title)
+    {
+        response.status(400).send("Title is required.");
+
+        return;
+    }
+
+    var newBook = new models.Book(request.body);
+    newBook.save();
+
+    response.send(newBook);
+});
+
+app.put("/books/:id", async (request, response) => {
+    var book = await models.Book.findById(parseInt(request.params.id));
+    if (!book)
+    {
+        response.status(400).send(`No book with id ${request.params.id}`);
+
+        return;
+    }
+
+    if (request.body.title)
+    {
+        book.title = request.body.title;
+    }
+
+    await book.save();
+
+    response.send(book);
+});
+
+app.listen(8080, () => {
+    console.log("Server started.");
+});
